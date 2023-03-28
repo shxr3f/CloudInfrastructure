e16fa157f4b55bf5d856621803f65d03ed5e99cf
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-03-28 15:49:17 +0800

    creating main.tf file

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform"
+    storage_account_name = "saterraformsharif"
+    container_name       = "tfstate"
+  }
+}
+ 
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+ 
