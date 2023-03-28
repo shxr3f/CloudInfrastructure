5f8b75da1d064026bd981d81ad8dbe4524d9d13d
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-03-28 17:04:36 +0800

    Using terraform.tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,9 @@
-variable "TFSTATE_KEY" {}
-
-
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform"
     storage_account_name = "saterraformsharif"
     container_name       = "tfstate"
-    key                  = variable.TFSTATE_KEY
+    key                  = "terraform.tfstate"
   }
 }
 
