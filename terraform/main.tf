0caa096a9905592eb81807effee37c662c988691
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-03-28 15:58:47 +0800

    Adding key

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "rg-terraform"
     storage_account_name = "saterraformsharif"
     container_name       = "tfstate"
+    key                  = "ttQtI3f57tTn4FJJzZRut32u5w5RNKdl/T9QQ1afCvegL7+d/1OnghZiGoLHNsWnSWOK0iX2UNNt+AStvflTgg=="
   }
 }
  
