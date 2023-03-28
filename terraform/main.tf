5698be5a9de41fa3d9908471b07dc8727ab135aa
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-03-28 16:42:18 +0800

    Retrieving key from github secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,12 @@
+variable "TFSTATE_KEY" {}
+
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform"
     storage_account_name = "saterraformsharif"
     container_name       = "tfstate"
+    key                  = variable.TFSTATE_KEY
   }
 }
 
