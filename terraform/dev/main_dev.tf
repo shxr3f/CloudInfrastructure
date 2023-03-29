0dea1527fea13069567f094c9141fe62ebbdddec
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-03-29 12:36:53 +0800

    Removing main_dev.tf

--- terraform/dev/main_dev.tf
+++
@@ -1,8 +0,0 @@
-terrafrom {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform"
-    storage_account_name = "saterraformsharif"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
