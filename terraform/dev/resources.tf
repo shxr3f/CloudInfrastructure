7a40e6b4a1d7327f60fe282f157eaf7fbb610bee
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-06-07 16:53:12 +0800

    Fixing location

--- terraform/dev/resources.tf
+++ terraform/dev/resources.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg-data-platform" {
 resource "azurerm_storage_account" "sa-data-platform" {
   name                     = "sharifstdataplatform"
   resource_group_name      = azurerm_resource_group.rg-data-platform.name
-  location                 = azurerm_resource_group.rg-data-platform.name
+  location                 = azurerm_resource_group.rg-data-platform.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
