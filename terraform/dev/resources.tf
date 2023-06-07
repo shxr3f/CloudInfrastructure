32694a22c07ad46ce851b52041f5de950f330cd4
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-06-07 16:46:24 +0800

    Create Common Resources

--- terraform/dev/resources.tf
+++ terraform/dev/resources.tf
@@ -1,7 +1,42 @@
-module "aks-cluster-dev" {
-  source = "../modules/aks"
+#Resource Group for Primary Resources
+resource "azurerm_resource_group" "rg-data-platform" {
+  name     = "rg-data-platform"
+  location = "southeastasia"
+}
 
-  location    = "southeastasia"
-  environment = "dev"
-  node_count  = 2
+# Storage Account
+resource "azurerm_storage_account" "sa-data-platform" {
+  name                     = "sharifstoragedataplatform"
+  resource_group_name      = azurerm_resource_group.rg-data-platform.name
+  location                 = azurerm_resource_group.rg-data-platform.name
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_storage_container" "landing-container" {
+  name                  = "landing"
+  storage_account_name  = azurerm_storage_account.sa-data-platform.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "archive-container" {
+  name                  = "archive"
+  storage_account_name  = azurerm_storage_account.sa-data-platform.name
+  container_access_type = "private"
+}
+
+# Databricks Workspace
+resource "azurerm_databricks_workspace" "databricks-workspace" {
+  name                = "databricks-workspace"
+  resource_group_name = azurerm_resource_group.rg-data-platform.name
+  location            = azurerm_resource_group.rg-data-platform.location
+  sku                 = "premium"
+
+  tags = {
+    Environment = "dev"
+  }
 }
