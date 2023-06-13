1885fee5f4c8c832a84c9d92d379d3dbf3620506
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-06-13 15:32:30 +0800

    Creating new Projects folder to store Data Projects

---
+++ terraform/modules/projects/variables.tf
@@ -0,0 +1,31 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "location" {
+  type        = string
+  default     = "southeastasia"
+  description = "Location of the resource group."
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Environment that AKS is to be Deployed"
+}
+
+variable "azurerm_databricks_workspace_scope" {
+  type        = string
+  description = "Scope of AZD Workspace"
+}
+
+variable "azurerm_databricks_workspace_url" {
+  type        = string
+  description = "URL of AZD Workspace"
+}
+
+variable "azurerm_databricks_workspace_id" {
+  type        = string
+  description = "ID of AZD Workspace"
+}
