e2580381c00dfe7d6c3c688393761d4342b714bb
Mohamed Sharif <mohamedsma@spgroup.com.sg>
2023-06-15 15:09:58 +0800

    Changing SKU

--- terraform/dev/resources.tf
+++ terraform/dev/resources.tf
@@ -93,7 +93,7 @@ resource "azurerm_postgresql_flexible_server" "db-server" {
 
   storage_mb = 32768
 
-  sku_name   = "B_Standard_B1s"
+  sku_name   = "B_Standard_B1ms"
   depends_on = [azurerm_private_dns_zone_virtual_network_link.db-nl]
 
 }
